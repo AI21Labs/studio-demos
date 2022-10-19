2e9590d123e125098e726cfec939b8cd868874a5
Chen Almagor <chena@ai21.com>
2022-10-19 12:17:36 +0300

    added multi pages

--- App.py
+++ App.py
@@ -1,7 +1,9 @@
 import streamlit as st
 from utils.studio_style import apply_studio_style
 if __name__ == '__main__':
-
+    st.set_page_config(
+        page_title="Welcome"
+    )
     apply_studio_style()
     st.title("Welcome to AI21 Studio")
     st.markdown("The following tabs allow you to navigate between demos that demonstrate our technology's capabilities." )
