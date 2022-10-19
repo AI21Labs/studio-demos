0bfc2105dada678c3e73c8f6c2f53829bc57cc7b
Chen Almagor <chena@ai21.com>
2022-10-19 12:05:14 +0300

    Revert "added multi pages"
    This reverts commit f52b760cfc29abfce4acd4a0086b76627beb7a4a.

--- Welcome_To_AI21_Studio.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-from utils.studio_style import apply_studio_style
-if __name__ == '__main__':
-
-    apply_studio_style()
-    st.title("Welcome to AI21 Studio")
-    st.markdown("The following tabs allow you to navigate between demos that demonstrate our technology's capabilities." )
-    st.markdown("To check out the brains behind the demos visit https://www.ai21.com/studio" )
