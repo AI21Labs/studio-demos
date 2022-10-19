c35a299e75c89c5e6c0678e9a9d7bc813a0f24df
Chen Almagor <chena@ai21.com>
2022-10-19 15:23:01 +0300

    font - with import

--- utils/studio_style.py
+++ utils/studio_style.py
@@ -3,15 +3,9 @@ import streamlit as st
 def apply_studio_style():
     st.markdown(
         """
-       
         <style>
-        @font-face {font-family: "Sofia Pro";
-    src: url("http://db.onlinewebfonts.com/t/9e00143409affcb46a1ae58634aa64be.eot"); /* IE9*/
-    src: url("http://db.onlinewebfonts.com/t/9e00143409affcb46a1ae58634aa64be.eot?#iefix") format("embedded-opentype"), /* IE6-IE8 */
-    url("http://db.onlinewebfonts.com/t/9e00143409affcb46a1ae58634aa64be.woff2") format("woff2"), /* chrome firefox */
-    url("http://db.onlinewebfonts.com/t/9e00143409affcb46a1ae58634aa64be.woff") format("woff"), /* chrome firefox */
-    url("http://db.onlinewebfonts.com/t/9e00143409affcb46a1ae58634aa64be.ttf") format("truetype"), /* chrome firefox opera Safari, Android, iOS 4.2+*/
-    url("http://db.onlinewebfonts.com/t/9e00143409affcb46a1ae58634aa64be.svg#Sofia Pro") format("svg"); /* iOS 4.1- */
+         @import url(http://db.onlinewebfonts.com/c/9e00143409affcb46a1ae58634aa64be?family=Sofia+Pro);
+         @font-face {font-family: "Sofia Pro";
 }
 
 			html, body, [class*="css"]  {
