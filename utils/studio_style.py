01c1d8d9137a9d98890e0b8413b9d52e91783450
Chen Almagor <chena@ai21.com>
2022-10-19 15:17:08 +0300

    font - again

--- utils/studio_style.py
+++ utils/studio_style.py
@@ -5,7 +5,14 @@ def apply_studio_style():
         """
        
         <style>
-        @import url(https://db.onlinewebfonts.com/c/9e00143409affcb46a1ae58634aa64be?family=Sofia+Pro);
+        @font-face {font-family: "Sofia Pro";
+    src: url("http://db.onlinewebfonts.com/t/9e00143409affcb46a1ae58634aa64be.eot"); /* IE9*/
+    src: url("http://db.onlinewebfonts.com/t/9e00143409affcb46a1ae58634aa64be.eot?#iefix") format("embedded-opentype"), /* IE6-IE8 */
+    url("http://db.onlinewebfonts.com/t/9e00143409affcb46a1ae58634aa64be.woff2") format("woff2"), /* chrome firefox */
+    url("http://db.onlinewebfonts.com/t/9e00143409affcb46a1ae58634aa64be.woff") format("woff"), /* chrome firefox */
+    url("http://db.onlinewebfonts.com/t/9e00143409affcb46a1ae58634aa64be.ttf") format("truetype"), /* chrome firefox opera Safari, Android, iOS 4.2+*/
+    url("http://db.onlinewebfonts.com/t/9e00143409affcb46a1ae58634aa64be.svg#Sofia Pro") format("svg"); /* iOS 4.1- */
+}
 
 			html, body, [class*="css"]  {
 			font-family: 'Sofia Pro';
