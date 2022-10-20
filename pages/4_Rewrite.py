c83c914c241e337ee1f565df79f30693ae9e504d
Tomer Asida <tomera@ai21.com>
2022-10-20 09:29:13 +0300

    basic implementation of a text area with left and right button - a lot of debug code and comments to clean

--- pages/4_Rewrite.py
+++ pages/4_Rewrite.py
@@ -1,3 +1,5 @@
+import random
+
 import requests
 import streamlit as st
 
@@ -5,8 +7,10 @@ from utils.completion import _full_url
 from utils.studio_style import apply_studio_style
 
 
-API_KEY = st.secrets['api-keys']['ai21-algo-team-prod']
-
+# API_KEY = st.secrets['api-keys']['ai21-algo-team-prod']
+import os
+API_KEY = os.getenv("API_KEY")
+DUMMIES_LIST = ["Dummy1", "Dummy2", "Dummy3", "Dummy4"]
 
 @st.cache(show_spinner=False)
 def rewrite(text, api_key, intent="general", span_start=0, span_end=None, env="production"):
@@ -27,6 +31,35 @@ def get_suggestions(text, intent='general', span_start=0, span_end=None):
     rewrite_resp = rewrite(text, intent=intent, span_start=span_start, span_end=span_end, api_key=API_KEY)
     rewritten_texts = [sug['text'] for sug in rewrite_resp['suggestions']]
     st.session_state["rewrite_rewritten_texts"] = rewritten_texts
+
+
+def show_next(ph):
+    curr_index = st.session_state["rewrite_curr_index"]
+    next_index = (curr_index + 1) % len(DUMMIES_LIST)
+    st.session_state["rewrite_curr_index"] = next_index
+    # st.session_state["rewrite_placeholder"].text_area(label='', value=DUMMIES_LIST[next_index], key="1")
+    ph.text_area(label='', value=DUMMIES_LIST[next_index], key="aaa")
+    # st.text_area(label='', value=DUMMIES_LIST[next_index], key="dummy")
+    # st.write(DUMMIES_LIST[next_index])
+    # new_comp_index = (st.session_state['generated_sections_data'][section_heading]["text_area_index"] + 1) % 5
+    # section_i_text = completions[arg_sorted_by_length[new_comp_index]]["data"]["text"]
+    # st.session_state['generated_sections_data'][section_heading]["text_area_index"] = new_comp_index
+    # st.session_state['generated_sections_data'][section_heading]["text_area_data"].text_area(label=section_heading,
+    #                                                                                          height=300,
+    #                                                                                          value=section_i_text,
+    #                                                                                          key=section_index)
+
+
+def show_prev(ph):
+    curr_index = st.session_state["rewrite_curr_index"]
+    prev_index = (curr_index - 1) % len(DUMMIES_LIST)
+    st.session_state["rewrite_curr_index"] = prev_index
+    # st.session_state["rewrite_placeholder"].text_area(label='', value=DUMMIES_LIST[prev_index], key="aaa")
+    ph.text_area(label='', value=DUMMIES_LIST[prev_index], key="aaa")
+    print(DUMMIES_LIST[prev_index])
+    print(st.session_state["rewrite_curr_index"])
+    # st.text_area(label='', value=DUMMIES_LIST[prev_index], key="dummy")
+    # st.write(DUMMIES_LIST[prev_index])
 
 
 if __name__ == '__main__':
@@ -53,3 +86,66 @@ if __name__ == '__main__':
     st.button(label="Rewrite", on_click=lambda: get_suggestions(text, intent=intent))
     if "rewrite_rewritten_texts" in st.session_state:
         st.markdown(body='* ' + '\n* '.join(st.session_state["rewrite_rewritten_texts"]))
+
+        # st.session_state["rewrite_placeholder"] = st.empty()
+        #
+        # st.session_state["rewrite_placeholder"].text_area(label='', value=DUMMIES_LIST[0], key="0")
+        # st.session_state["rewrite_curr_index"] = 0
+        # col1, col2, col3, col4 = st.columns([0.5, 0.5, 4, 4])
+        #
+        # with col1:
+        #     st.button("<", on_click=show_prev)
+        # with col2:
+        #     st.button(">", on_click=show_next)
+
+        # with st.empty():
+        #     st.text_area(DUMMIES_LIST[0])
+        #     st.session_state["rewrite_curr_index"] = 0
+        #     col1, col2, col3, col4 = st.columns([0.5, 0.5, 4, 4])
+        #
+        #     with col1:
+        #         st.button("<", on_click=show_prev)
+        #     with col2:
+        #         st.button(">", on_click=show_next)
+
+    ph = st.empty()
+    st.session_state["rewrite_placeholder"] = st.empty()
+    if "rewrite_curr_index" not in st.session_state:
+        st.session_state["rewrite_curr_index"] = 0
+    curr_index = st.session_state["rewrite_curr_index"]
+    st.session_state["rewrite_placeholder"].text_area(label='', value=DUMMIES_LIST[curr_index])
+    # ph.text_area(label="", value=DUMMIES_LIST[0], key="aaa")
+    st.write(st.session_state)
+    print(st.session_state["rewrite_curr_index"])
+
+    col1, col2, col3, col4 = st.columns([0.5, 0.5, 4, 4])
+    with col1:
+        st.button("<", on_click=lambda: show_prev(ph))
+    with col2:
+        st.button(">", on_click=lambda: show_next(ph))
+    print(st.session_state["rewrite_curr_index"])
+
+    # for seconds in range(60):
+    #     next_index = (st.session_state["rewrite_curr_index"] + 1) % len(DUMMIES_LIST)
+    #     st.session_state["rewrite_curr_index"] = next_index
+    #     ph.text_area(label="", value=DUMMIES_LIST[next_index], key=str(random.random()))
+    #     import time
+    #     time.sleep(1)
+    # ph.write("✔️ 1 minute over!")
+
+    # with st.empty():
+        # for seconds in range(60):
+        #     st.text_area(label="", value=f"⏳ {seconds} seconds have passed")
+        #     import time
+        #     time.sleep(1)
+        # st.write("✔️ 1 minute over!")
+    # Replace the text with a chart:
+    # placeholder.line_chart({"data": [1, 5, 2, 6]})
+
+    # Replace the chart with several elements:
+    # with placeholder.container():
+    #     st.write("This is one element")
+    #     st.write("This is another")
+
+    # Clear all those elements:
+    # placeholder.empty()
