12cc911fc4bfc30fa341772003117e3a8e489974
Idan Rejwan <idanr@ai21.com>
2022-12-12 16:44:22 +0200

    change copy

--- Welcome.py
+++ Welcome.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
         page_title="Welcome"
     )
     apply_studio_style()
-    st.title("Welcome to AI21 Studio")
+    st.title("Welcome to AI21 Studio demos")
     st.markdown("Experience the incredible power of large language models first-hand. With these demos, you can explore a variety of unique use cases that showcase what our sophisticated technology is truly capable of. From instant content generation to a paraphraser that can rewrite any text, the world of AI text generation will be at your fingertips." )
     st.markdown("Check out the brains behind the demos here: https://www.ai21.com/studio")
     st.markdown("Please note that this is a limited demonstration of AI21 Studio's capabilities. If you're interested in learning more, contact us at studio@ai21.com")
